dcb4a3d9bdfeab14470ad780132db99a0dc64459
Arya-B4133 <192424133.simats@saveetha.com>
2025-04-29 09:02:46 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the value:"))
+i=1
+fact=1
+while (i<=n):
+    fact=fact*i
+    i=i+1
+print('The factorial of',n,'is',fact)
+    
+    
---
+++ Grt_of_3.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+c=int(input("Enter the number:"))
+if (a>b):
+    if(a>c):
+        print(a," is the greatest number")
+    else:
+        print(c," is the greatest number")
+if (b>c):
+    (b," is the greastest number")
+else:
+    (c," is the greatest number")
